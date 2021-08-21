96f5be62ba1d3532800064b004e06678c1b23476
I-am-Myxi <im.myxi@yandex.com>
2021-08-21 12:40:44 +0530

    fixed a bug and added a command to unload

--- cogs/admin_only.py
+++ cogs/admin_only.py
@@ -1,7 +1,7 @@
 import discord ; from discord.ext import commands
 import os, pkgutil
 from extras.easy_embed import easyembed
-class AdminOnly(commands.Cog):
+class AdminOnly(commands.Cog, command_attrs=dict(hidden=True)):
     def __init__(self, bot) -> None:
         self.bot = bot
     async def cog_check(self, ctx):
@@ -39,19 +39,19 @@ class AdminOnly(commands.Cog):
         for num, extension in enumerate(cogs):
             if extension.startswith(name):
                 try:
-                    await ctx.send(embed = easyembed.error(
+                    msg = await ctx.send(embed = easyembed.error(
                         f'Reloading cog "{extension.capitalize()}"',
                         "", ctx
                     ))
                     self.bot.reload_extension(f'cogs.{extension}')
                 except Exception: 
-                    await ctx.send(embed = easyembed.error(
+                    await msg.edit(embed = easyembed.error(
                         "Couldn't reload",
                         "", ctx
                     ))
                     return
                 else:
-                    await ctx.send(embed = easyembed.error(
+                    await msg.edit(embed = easyembed.error(
                         f'Cog "{extension.capitalize()}" reloaded.',
                         "", ctx
                     ))
@@ -62,7 +62,63 @@ class AdminOnly(commands.Cog):
                         'Not found', '', ctx
                     ))
                     return
-
-
+    @commands.command()
+    async def load(self, ctx, name: str):
+        cogs = [extension.name for extension in pkgutil.iter_modules(['cogs']) if extension.name != 'admin_only']
+        for num, extension in enumerate(cogs):
+            if extension.startswith(name):
+                try:
+                    msg = await ctx.send(embed = easyembed.error(
+                        f'Loading cog "{extension.capitalize()}"',
+                        "", ctx
+                    ))
+                    self.bot.load_extension(f'cogs.{extension}')
+                except Exception as err:
+                    await msg.edit(embed = easyembed.error(
+                        "Either it's already loaded or I couldn't load it.",
+                        "", ctx
+                    ))
+                    return
+                else:
+                    await msg.edit(embed = easyembed.error(
+                        f'Cog "{extension.capitalize()}" loaded.',
+                        "", ctx
+                    ))
+                    return
+            else:
+                if num == (len(cogs) - 1):
+                    await ctx.send(embed = easyembed.simple(
+                        'Not found', '', ctx
+                    ))
+                    return
+    @commands.command()
+    async def unload(self, ctx, name: str):
+        cogs = [extension.name for extension in pkgutil.iter_modules(['cogs']) if extension.name != 'admin_only']
+        for num, extension in enumerate(cogs):
+            if extension.startswith(name):
+                try:
+                    msg = await ctx.send(embed = easyembed.error(
+                        f'Unloading cog "{extension.capitalize()}"',
+                        "", ctx
+                    ))
+                    self.bot.unload_extension(f'cogs.{extension}')
+                except Exception as err:
+                    await msg.edit(embed = easyembed.error(
+                        "Either it's not loaded yet or I couldn't unload it.",
+                        "", ctx
+                    ))
+                    return
+                else:
+                    await msg.edit(embed = easyembed.error(
+                        f'Cog "{extension.capitalize()}" unloaded.',
+                        "", ctx
+                    ))
+                    return
+            else:
+                if num == (len(cogs) - 1):
+                    await ctx.send(embed = easyembed.simple(
+                        'Not found', '', ctx
+                    ))
+                    return
 def setup(bot):
     bot.add_cog(AdminOnly(bot))
